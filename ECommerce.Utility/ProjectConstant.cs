7841c390f45e25cd28b709fc2026c86a0735deae
sirems <siremsoydan@icloud.com>
2020-05-26 01:13:58 +0300

    Create Areas Sections(Admin and Customer)

---
+++ ECommerce.Utility/ProjectConstant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Utility
+{
+    public static class ProjectConstant
+    {
+        public const string ResultNotFound = "Data Not Found";
+    }
+}
