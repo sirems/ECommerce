eaaff8590af03ae56ad9853dfb9288aeb667eef5
sirems <siremsoydan@icloud.com>
2020-05-26 13:42:28 +0300

    Repository

---
+++ ECommerce.DataAccess/IMainRepository/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ECommerce.DataAccess.IMainRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //id vererek tek kayıt getirme
+        T Get(int id); 
+            
+        //çoklu kayıt getirme, belirli bir filtreleme ile,tablo birleştirerek vs
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null);
+
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter = null,
+            string includeProperties = null);
+
+        void Add(T entity);
+        void Remove(int id);
+        void Remove(T entity);
+        //çoklu kayıt silme
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
---
+++ ECommerce.DataAccess/MainRepository/Repository.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using ECommerce.DataAccess.Data;
+using ECommerce.DataAccess.IMainRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.DataAccess.MainRepository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet; //bu program içinde eriş 
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public T Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                //("","","","","Example") böyle bir kayıt varsa;
+                foreach (var item in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(item);
+                }
+            }
+            //temiz data elde edilsin diye order by sona bırakıldı
+            //Category.filter.Include(Product)
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var item in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(item);
+                }
+            }
+            return query.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Generic Added Entity
+        /// </summary>
+        /// <param name="entity"></param>
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Remove(int id)
+        {
+            T entitiy = dbSet.Find(id);
+            Remove(entitiy);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+        /// <summary>
+        /// Remove Range Multiple Entity
+        /// </summary>
+        /// <param name="entity"></param>
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
