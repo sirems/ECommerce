89e576bd2be4f2ae474631e06d537ad7bd1f9039
sirems <siremsoydan@icloud.com>
2020-06-27 18:02:24 +0300

    User management

---
+++ ECommerce/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.DataAccess.Data;
+using ECommerce.DataAccess.IMainRepository;
+using ECommerce.Models.DbModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        #region Variables
+        //private readonly IUnitOfWork _uow;
+        private readonly ApplicationDbContext _db;
+        #endregion
+
+        #region CTOR
+        //ınjection 
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        } 
+        #endregion
+
+        #region Actions
+        public IActionResult Index()
+        {
+            return View();
+        } 
+        #endregion
+
+        #region API CALLS
+
+        public IActionResult GetAll()
+        {
+            var userList = _db.ApplicationUsers.Include(c => c.Company).ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            foreach (var user in userList)
+            {
+                var roleId = userRole.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+
+                if (user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = string.Empty
+                    };
+                }
+            }
+            return Json(new { data = userList });
+        }
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+            var data = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if (data == null)
+                return Json(new { success = false, message = "Error while locking/unlocking" });
+
+            if (data.LockoutEnd != null && data.LockoutEnd > DateTime.Now)
+                data.LockoutEnd = DateTime.Now;
+            else
+                data.LockoutEnd = DateTime.Now.AddYears(10);
+
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Operation Successfully" });
+        }
+
+
+        #endregion
+
+
+    }
+}
